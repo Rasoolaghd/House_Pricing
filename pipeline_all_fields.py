2e73400986ce5ae4d06673ceba292a07aafd4071
RASOOL <rasool@RASOOLs-iMac.local>
2019-11-01 23:15:35 +0900

    custom transformer added

--- pipeline_all_fields.py
+++ pipeline_all_fields.py
@@ -1,1 +1,17 @@
 
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# a transformer that gets the desired attributes and converting Dataframe to numpy array.
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+num_attributes = list(housing_num)
